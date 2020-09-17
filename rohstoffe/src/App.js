b94c10ae8505ccfb5a761a0fdb7092ac5fc3c7ed
Snoppy0817 <Snoppy0817@gmail.com>
2020-09-17 12:07:40 -0500

    Pantalla de bienvenida completa, sin funcionalidad en botónes

--- rohstoffe/src/App.js
+++ rohstoffe/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './components/logom.png';
+import './App copy'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <img src={logo} className="logo" alt="logo" />
+        <div className="App-buttons">
+          <button type="button" class="btn btn-tcc btn-lg btn-color btn-bg-color btn-sm col-xs-2 btn-tcc-spacing">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Registrarse
+          </button>
+          <button type="button" class="btn btn-tcc btn-color btn-bg-color btn-sm col-xs-2 btn-tcc-spacing" href='./App copy.js'>
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Iniciar Sesión
+          </button>
+        </div>
+
       </header>
+
     </div>
   );
 }
