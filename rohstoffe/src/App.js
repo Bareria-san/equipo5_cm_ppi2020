c1015e11f8a5500bf5af7968ec9625cb9ce1d64e
Miguel Padilla <miguepadilla2016@gmail.com>
2020-09-22 21:40:58 -0500

    Valeria, ya hice la tarea de API-Óscar del server

--- rohstoffe/src/App.js
+++ rohstoffe/src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './imagenes/perfume-calvin.jpg';
 import './App.css';
+import bm from './imagenes/Bm.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="nav-false">
+      <img src={bm} className="nav-false" alt="logo" />
+      <p>
+      Favoritos
+      </p>
+
+      <div class="Texto-desk">
+        <p class="desk-title">
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="App">
+        <header className="App-header">
+
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <div class="Texto-desk">
+            <p class="desk-title">
+              Pachuli Calvin clein.
+        </p>
+          </div>
+          <a
+            className="App-link"
+            href="https://marvelapp.com/prototype/f1336ah/screen/72156131"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+
+          </a>
+          <div className="nav-false">
+      <img src={bm} className="nav-false" alt="logo" />
+
+
     </div>
+    </header>
+  </div>
+</div>
   );
 }
 
