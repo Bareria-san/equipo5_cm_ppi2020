18121ebbeab2b4e4010ad8aef887e8ee7e40a1a1
Cristian-Usme <cristian1213u@gmail.com>
2020-10-15 08:42:37 -0500

    Pantallas de inicio de sesión y registro

---
+++ rohstoffe/src/Login/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './Login.css'
+import Logo from '../assets/img/Logo.png';
+import Title from './components/Title/Title'
+import Label from './components/Label/Label'
+import Input from './components/Input/Input'
+
+
+const Login = () => {
+
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    function handleChange(name, value) {
+        if (name === 'usuario') {
+            setUser(value)
+        } else {
+            setPassword(value)
+        }
+    };
+
+    function handleSumit() {
+        let account = { user, password }
+        if (account) {
+            console.log('account:', account)
+        }
+    };
+    function handleCancel() {
+        let cancel = 'Cancelando operación'
+        if (cancel) {
+            console.log(cancel)
+        }
+    };
+
+    return (
+        <div className='login-container'>
+            <img src={Logo} className='Logo' alt='Logo' />
+            <Title text='Inicia sesión' />
+            <Label text='Nombre de usuario' />
+            <Input
+                attribute={{
+                    id: 'usuario',
+                    name: 'usuario',
+                    type: 'text',
+                    placeholder: 'Ingrese su usuario'
+                }}
+                handleChange={handleChange}
+            />
+            <Label text='Contraseña' />
+            <Input
+                attribute={{
+                    id: 'contraseña',
+                    name: 'contraseña',
+                    type: 'password',
+                    placeholder: 'Ingrese su contraseña'
+                }}
+                handleChange={handleChange}
+            />
+            <button onClick={handleSumit} className='Ok'>
+                Ingresar
+            </button>
+            <button onClick={handleCancel} className='cancel'>
+                Cancelar
+            </button>
+            <Label text='¿Aún no tiene cuenta?'/>
+            <button className= 'here'>Registrate aquí</button>
+        </div>
+    )
+
+}
+
+export default Login;
---
+++ rohstoffe/src/Login/SingUp.js
@@ -0,0 +1,91 @@
+import React, {useState} from 'react';
+import Title from './components/Title/Title'
+import Logo from '../assets/img/Logo.png'
+import './SingUp.css' 
+import Label from './components/Label/Label';
+import Input from './components/Input/Input';
+//import Label from './components/Label/Label'
+const SingUp  = () => {
+
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleChange(name, value) {
+        if (name === 'usuario') {
+            setUser(value)
+        } else {
+            setPassword(value)
+        }
+    };
+
+    function handleSumit() {
+        let account = { user, password }
+        if (account) {
+            console.log('account:', account)
+        }
+    };
+    function handleCancel() {
+        let cancel = 'Cancelando operación'
+        if (cancel) {
+            console.log(cancel)
+        }
+    };
+    return (
+    <div className = 'register-container'>
+        <img src={Logo} className='Logo' alt='Logo' />
+        <Title text='Registrate' />
+        <Label text= 'Nombre' />
+        <Input 
+        attribute={{
+            id: 'usuario',
+            name: 'usuario',
+            type: 'text',
+            placeholder: 'Ingrese su usuario'
+        }}
+        handleChange={handleChange}
+        />
+        <Label text= 'Apellido'/> 
+        <Input 
+        attribute={{
+            id: 'usuario',
+            name: 'usuario',
+            type: 'text',
+            placeholder: 'Ingrese su usuario'
+        }}
+        handleChange={handleChange}
+        />
+        <Label text= 'Correo electronico' />
+        <Input 
+        attribute={{
+            id: 'usuario',
+            name: 'usuario',
+            type: 'text',
+            placeholder: 'Ingrese su correo'
+        }}
+        handleChange={handleChange}
+        />
+        <Label text= 'Contraseña' />
+        <Input 
+        attribute={{
+            id: 'contraseña',
+            name: 'usuario',
+            type: 'password',
+            placeholder: 'Digite su contraseña'
+        }}
+        handleChange={handleChange}
+        />
+        <Label text= 'Confirme su contraseña' />
+        <Input 
+        attribute={{
+            id: 'Confirm-contraseña',
+            name: 'Rcontraseña',
+            type: 'password',
+            placeholder: 'Digite nuevamente su contraseña'
+        }}
+        handleChange={handleChange}
+        />
+    </div>
+    )
+}
+
+export default SingUp;
