85505ee8a0c834b9130714f9730b3e5f89bbdc24
AlejoRo <alejorodg@outlook.com>
2020-10-20 09:14:27 -0500

    mitad del crud

--- backend-rohstoffe/index.js
+++ backend-rohstoffe/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require("path");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan('dev'))
+app.use(express.json())
 
 app.get('/',(req,res) => {
     res.send('Servidor Rohstoffe equipo 5 Activo');
 });
 
-app.listen(3000, () => {
-  console.log('server on port 3000');
-});
+app.set("puerto", 3000)
+
+app.listen(app.get("puerto"), () => {
+  console.log(`Servidor Rohstoffe corriendo en el puerto ${app.get("puerto")}`)
+})
---
+++ backend-rohstoffe/routesco/inicio.js
@@ -0,0 +1,21 @@
+const {Router} = require("express")
+const router = Router()
+const fs = require("fs")
+const FileInicio = fs.readFileSync('./productos.json', 'utf-8')
+const JSONInicio = JSON.parse(FileInicio)
+
+router.get("/inicio", (req, res) => {
+  res.json(JSONInicio)
+})
+
+router.get("/inicio/:id", (req,res) => {
+    let id = req.params.id
+    let ProductoEncontrado = JSONInicio.find(producto => producto.id == id)
+  
+    if(ProductoEncontrado != undefined)
+      res.status(200).json(ProductoEncontrado)
+    else
+      res.json(`El producto con el ID ${id} no existe`)
+  })
+
+module.exports = router
