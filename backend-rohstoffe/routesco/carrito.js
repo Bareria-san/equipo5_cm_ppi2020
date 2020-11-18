f2c2f2424890b164c6f89d52e989d7777fefa7f4
Valeria Castañeda <valeriaCe526@gmail.com>
2020-11-18 00:46:56 +0000

    Parte de Valeria :3

--- backend-rohstoffe/routesco/carrito.js
+++ backend-rohstoffe/routesco/carrito.js
@@ -1,61 +1,40 @@
-const {Router} = require("express")
+const { Router } = require("express")
 const router = Router()
-const {connection} = require("./../db/mysql_pool")
-//connection.connect() 
+const { connection } = require("./../db/mysql_pool")
 const fs = require("fs")
 const FileCarrito = fs.readFileSync('./carrito.json', 'utf-8')
 const JSONCarrito = JSON.parse(FileCarrito)
 
-router.get("/carritodecompra", (req, res) => {
+router.post('/carritodecompra', (req, res) => {
   try{
-    connection.query("SELECT * FROM `carrito_de_compra`",(error,rows,fields)=>{
+    const {
+      cantidad,
+      valor_total,
+      direccion_entrega,
+      monto
+    } = req.body    
+    const SQL = `INSERT INTO carrito_de_compra(cantidad, valor_total,direccion_entrega, monto)
+                  VALUES(?,?,?,?)`
+    const parametros = [cantidad, valor_total, direccion_entrega, monto]
+    connection.query(SQL, parametros, (error, results, fields) => {
       if(error){
-        res.status(502).json("error de base de datos")
-    }
-     res.json(rows)
-    })
-    }catch(error){
-      console.log("Este es el error: ")
-  }
-})
-
-router.put("/carritodecompra/:id_carrito", (req, res) => {
- try{
-     const id_carrito = req.params.id
-     const {
-              cantidad,
-              valor_total,
-              direccion_entrega,
-              monto
-
-     } = req.body
-
-     connection.query(`UPDATE carrito_de_compra
-     SET cantidad = '${cantidad}'
-      WHERE id_carrito = ?`,[cantidad,id_carrito],(error,resulset) => {
-        console.log(resulset)
         console.log(error)
-      })
-    // console.log(id_carrito)
-    }catch(error){
-      res.status(502).json({mensaje: "Error en el servidor", error : true})
+        res.status(502).json({mensaje : 'Error ejecutando la consulta.'})
+      }else{
+        console.log(results)
+        res.status(201).json({
+                              id : results.insertId,
+                              cantidad : cantidad,
+                              valor_total : valor_total,
+                              direccion_entrega : direccion_entrega,
+                              monto : monto,
+                              })
+      }
+    })
+  }catch(error){
+    res.status(502).json({mensaje:"Error en el servidor"})
   }
 })
 
-router.post("/carritodecompra", (req, res) => {
-    let id = JSONCarrito.length + 1
-    let {precio, cantidad, total, direccion, monto} = req.body
-    let nuevoCarrito = {
-      "id" : id,
-      "precio": precio,
-      "cantidad": cantidad,
-      "total": total,
-      "direccion": direccion, 
-      "monto": monto
-    } 
-    JSONCarrito.push(nuevoCarrito)
-    fs.writeFileSync('./carrito.json', JSON.stringify(JSONCarrito), 'utf-8')
-    res.status(201).json(nuevoCarrito)
-  })
 
 module.exports = router
--- backend-rohstoffe/routesem/pedidos.js
+++ backend-rohstoffe/routesem/pedidos.js
@@ -1,32 +1,32 @@
 
-const {Router} = require("express")
+const { Router } = require("express")
 const router = Router();
-const {connection} = require("./../db/mysql_pool")
+const { connection } = require("./../db/mysql_pool")
 
-router.get('/pedidos',(req,res)=>{
-  try{
-    connection.query('SELECT * FROM carrito_de_compra',(err, row, fiels)=>{
-      if(!err){
+router.get('/pedidos', (req, res) => {
+  try {
+    connection.query('SELECT * FROM carrito_de_compra', (err, row, fiels) => {
+      if (!err) {
         console.log(err)
       }
       res.json(row)
     })
-  }catch(error){
-    res.status(502).json({mensaje:"Error en la base de datos"})
+  } catch (error) {
+    res.status(502).json({ mensaje: "Error en la base de datos" })
   }
 })
 
-router.get('/pedidos/:id',(req,res)=>{
-  let {id} = req.params;
-  try{
-    connection.query('SELECT * FROM carrito_de_compra WHERE id_carrito = ?',[id],(err, row, fields)=>{
-      if(!err){
+router.get('/pedidos/:id', (req, res) => {
+  let { id } = req.params;
+  try {
+    connection.query('SELECT * FROM carrito_de_compra WHERE id_carrito = ?', [id], (err, row, fields) => {
+      if (!err) {
         console.log(err)
       }
       res.json(row[0])
     })
-  }catch(error){
-    res.status(502).json({mensaje:"Error en la base de datos"})
+  } catch (error) {
+    res.status(502).json({ mensaje: "Error en la base de datos" })
   }
 })
 
