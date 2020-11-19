9331b99085eea81032df6374104123e679c98bf1
Derly Viviana Murcia Serrano <derlyvivianamurcia@gmail.com>
2020-11-19 17:42:34 +0000

    deploy

--- backend-rohstoffe/db/mysql.js
+++ backend-rohstoffe/db/mysql.js
@@ -1,7 +1,7 @@
 const mysql = require ('mysql')
 const connection = mysql.createConnection({
 
-  port: 3306,
+  port: process.env.PORT,
   host:process.env.HOST_DB,
   user: process.env.USER_DB,
   password: process.env.PASSWORD_DB,
