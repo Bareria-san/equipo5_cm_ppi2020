5d4d6438f152b9ee82d1318b0c06864fd74119a3
AlejoRo <alejorodg@outlook.com>
2020-11-27 05:35:05 -0500

    metodos 2 final

--- backend-rohstoffe/routeslog/registro.js
+++ backend-rohstoffe/routeslog/registro.js
@@ -3,28 +3,28 @@ const router = Router()
 const {connection} = require("./../db/mysql_pool")
 
 router.post('/login',(req,res)=>{
-const {nombre_completo, correo, contrasena, id_carrito} = req.body;
-let datacompra = [nombre_completo, correo, contrasena, id_carrito];
-let newcompra = 'INSERT INTO compradores(nombre_completo, correo, contrasena, id_carrito) VALUES (?,?,?,?)';
+const {nombre_completo, correo, contrasena} = req.body;
+let datacompra = [nombre_completo, correo, contrasena];
+let newcompra = 'INSERT INTO compradores(nombre_completo, correo, contrasena) VALUES (?,?,?)';
 connection.query(newcompra, datacompra, (err, results, fields)=>{
   if(err){
     return console.error(err.message)
   }
   console.log(results)
-  res.json({ message:`Pedido creado <[:)!!`, })
+  res.json({ message:`comprador agregado`, })
 })
 })
 
 router.post('/loginem',(req,res)=>{
-const {nombre_empresa,nombre_propetario, correo_empresa, contrasena_empresa, direccion, telefono} = req.body;
-let dataempresa = [nombre_empresa,nombre_propetario, correo_empresa, contrasena_empresa, direccion, telefono];
-let newempresa = 'INSERT INTO empresas(nombre_empresa,nombre_propetario, correo_empresa, contrasena_empresa, direccion, telefono) VALUES (?,?,?,?,?,?)';
+const {nombre_empresa,nombre_propetario, correo_empresa, contrasena_empresa, telefono} = req.body;
+let dataempresa = [nombre_empresa,nombre_propetario, correo_empresa, contrasena_empresa, telefono];
+let newempresa = 'INSERT INTO empresas(nombre_empresa,nombre_propetario, correo_empresa, contrasena_empresa, telefono) VALUES (?,?,?,?,?)';
 connection.query(newempresa, dataempresa, (err, results, fields)=>{
   if(err){
     return console.error(err.message)
   }
   console.log(results)
-  res.json({ message:`Pedido creado <[:)!!`, })
+  res.json({ message:`empresa creada`, })
 })
 })
 
