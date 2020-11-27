507c9371a46b486d14990c49c06ac291efe6507b
Cristian-Usme <cristian1213u@gmail.com>
2020-11-27 01:25:17 -0500

    Una más

--- rohstoffe/src/pages/historial.jsx
+++ rohstoffe/src/pages/historial.jsx
@@ -25,7 +25,7 @@ const Historial = () => {
 
     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
       <div class="card-body">
-          Hellowda
+          Comprada el 26/01/2020
           <button className= 'volveracomprar'>
            Volver a comprar
        </button>
@@ -37,13 +37,13 @@ const Historial = () => {
     <div class="card-header" id="headingTwo">
       <h2 class="mb-0">
         <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Otra Calvin Klein que como que le gustó
+          Lacoste Red
         </button>
       </h2>
     </div>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
       <div class="card-body">
-          Hellowda
+          Precio:
       <button className= 'volveracomprar'>
            Volver a comprar
        </button>
@@ -55,13 +55,13 @@ const Historial = () => {
     <div class="card-header" id="headingThree">
       <h2 class="mb-0">
         <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          La misma de arriba
+          Calvin Klein
         </button>
       </h2>
     </div>
     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
       <div class="card-body">
-          Hellowda
+          Comprada el 10/09/2020
       <button className= 'volveracomprar'>
            Volver a comprar
        </button>
